03f79a1a75ad7df2a33c31fb3041d41098017dfb
Carl Almazan <calmazan561@gmail.com>
2023-01-22 09:44:21 -0500

    Update top_profits_throughout_stores.sql

--- maven_mexican_toy_sales_analytics/top_profits_throughout_stores.sql
+++ maven_mexican_toy_sales_analytics/top_profits_throughout_stores.sql
@@ -1,6 +1,12 @@
--- shows whether it's the same when store location is a factor 
-SELECT  `Store_Location`, `Product_Category`, `profit`
- FROM toy_sales.join_one
- GROUP BY `Store_Location`, `Product_Category`, `profit` 
- ORDER BY `Store_location` ,`profit` DESC; 
+-- takes sum of the profit of each product cateogry 
+WITH `cte` AS (
+SELECT `Store_Location`, `Product_Category`, SUM(`profit`) AS `total_profit` 
+FROM toy_sales.join_one  
+GROUP BY `Store_Location`, `Product_Category`) 
+
+-- takes the category with the highest profit value from each location. It also returns the profit as well. 
+SELECT `Store_Location`, `Product_Category`, `total_profit` 
+FROM `cte` 
+WHERE (`Store_Location`, `total_profit`) IN (SELECT `Store_Location`, MAX(`total_profit`) FROM `cte` GROUP BY `Store_Location`) 
+ORDER BY `Store_Location` ASC; 
 
